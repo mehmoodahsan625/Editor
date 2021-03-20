b9e9c01574d83a2aa54c04da771742fe312fd31f
ahsan mehmood <mehmoodahsan625@gmail.com>
2021-03-20 14:43:20 +0500

    Add project files.

---
+++ WebApplication3/Controllers/FirstController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Editor.Models;
+
+namespace Editor.Controllers
+{
+    public class FirstController : Controller
+    {
+        [HttpGet]
+        public IActionResult Editor()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Editor(string editor)
+        {
+            pdf modeldata = new pdf();
+                modeldata.body = editor;
+                return new ViewAsPdf("_pdfPartial", modeldata)
+                {
+                    FileName = "MyPdfFile.pdf"
+                };
+            
+        }
+    }
+}
---
+++ WebApplication3/Models/pdf.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Editor.Models
+{
+    public class pdf
+    {
+        public string body { get; set; }
+    }
+}
